c847e44870504b3498cde0f2bd0f9c93c534c3e7
FREE <84310382+FreeSob@users.noreply.github.com>
2024-11-18 21:22:39 +0800

    Add files via upload

--- tcpfilesender.cpp
+++ tcpfilesender.cpp
@@ -14,9 +14,9 @@ TcpFileSender::TcpFileSender(QWidget *parent)
     ipAddressInput = new QLineEdit;
     ipAddressInput->setPlaceholderText(QStringLiteral("例如：127.0.0.1"));
 
-    ipPortLabel = new QLabel(QStringLiteral("IP 位址："));
+    ipPortLabel = new QLabel(QStringLiteral("PORT 位址："));
     ipPortInput = new QLineEdit;
-    ipPortInput->setPlaceholderText(QStringLiteral("例如：8080"));
+    ipPortInput->setPlaceholderText(QStringLiteral("例如：8888"));
 
     startButton = new QPushButton(QStringLiteral("開始"));
     quitButton = new QPushButton(QStringLiteral("退出"));
